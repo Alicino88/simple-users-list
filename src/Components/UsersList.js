681019ba9d24317558fccf373065ffe30780cbf5
Alice Moretti <ali.moret88@gmail.com>
2022-08-29 15:21:07 +0300

    Added UsersList component

---
+++ src/Components/UsersList.js
@@ -0,0 +1,16 @@
+import Card from "./UI/Card";
+function UsersList(props) {
+  return (
+    <Card>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export default UsersList;
