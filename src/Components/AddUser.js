f11c6079be2d514cbba926e2e92dec878959c86c
Alice Moretti <ali.moret88@gmail.com>
2022-08-29 14:40:22 +0300

    Added AddUser component that collects users data, & clears fields

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,15 +1,44 @@
+import Card from "./UI/Card";
+import Button from "./UI/Button";
+import { useState } from "react";
+
 function AddUser() {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const userSubmitHandler = (e) => {
     e.preventDefault();
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
   return (
-    <form onSubmit={userSubmitHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="texts"></input>
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={userSubmitHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="texts"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        ></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        ></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 }
 
