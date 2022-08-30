9f76e1bf4e3eb7f79b2898c4ea23c2fd2fad3693
Alice Moretti <ali.moret88@gmail.com>
2022-08-30 14:21:19 +0300

    Added style to Card component

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -1,7 +1,9 @@
 import classes from "./Card.module.css";
-
+/*CSS modules: we use template literals to apply two classes-> card class and the class coming as a prop from AddUser*/
 function Card(props) {
-  return <div className={classes.card}>{props.children}</div>;
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
 }
 
 export default Card;
