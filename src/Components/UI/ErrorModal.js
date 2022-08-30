4b65294e910c34290a98c98f9166b2e4f72de07c
Alice Moretti <ali.moret88@gmail.com>
2022-08-30 15:35:57 +0300

    Added style for ErrorModal

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,19 +1,23 @@
 import Card from "./Card";
 import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
 function ErrorModal(props) {
   return (
-    <Card>
-      <header>
-        <h2>{props.title}</h2>
-      </header>
-      <div>
-        <p>{props.message}</p>
-      </div>
-      <footer>
-        <Button>Ok</Button>
-      </footer>
-    </Card>
+    <div>
+      <div className={classes.backdrop} onClick={props.onCloseModal} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onCloseModal}>Ok</Button>
+        </footer>
+      </Card>
+    </div>
   );
 }
 
