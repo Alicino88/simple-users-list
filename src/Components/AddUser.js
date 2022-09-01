7b1de832175ea1c3edaa15efd73d9087a5bca848
Alice Moretti <ali.moret88@gmail.com>
2022-09-01 13:51:29 +0300

    Added Refs for form inputs

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,24 +1,33 @@
 import Card from "./UI/Card";
 import Button from "./UI/Button";
 import { useState } from "react";
+import { useRef } from "react";
 import ErrorModal from "./UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
+  /*Below we have removed the useState logic and replaced with Refs: in this way we don't update the component state at every keystroke to store the actual value:
+  /*
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  */
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState("");
   const userSubmitHandler = (e) => {
     e.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
 
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "The input is not valid",
         message: "please fill in both username and age fields",
       });
       return;
     }
-    if (enteredAge < 1) {
+    if (enteredUserAge < 1) {
       setError({
         title: "The age is not valid",
         message: "Nobody is younger than 0, please add a valid age (>0)",
@@ -27,18 +36,26 @@ function AddUser(props) {
     }
     /*console.log("hello");
     console.log(enteredUsername, enteredAge);*/
-    props.onAddUsers(enteredUsername, enteredAge);
+    props.onAddUsers(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+
+    /*
     setEnteredUsername("");
     setEnteredAge("");
+    */
   };
 
+  /*
   const usernameChangeHandler = (e) => {
     setEnteredUsername(e.target.value);
   };
+  */
 
+  /*
   const ageChangeHandler = (e) => {
     setEnteredAge(e.target.value);
-  };
+  };*/
 
   const errorHandler = () => {
     setError(null);
@@ -56,19 +73,9 @@ function AddUser(props) {
       <Card className={classes.input}>
         <form onSubmit={userSubmitHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="texts"
-            onChange={usernameChangeHandler}
-            value={enteredUsername}
-          ></input>
+          <input id="username" type="texts" ref={nameInputRef}></input>
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            onChange={ageChangeHandler}
-            value={enteredAge}
-          ></input>
+          <input id="age" type="number" ref={ageInputRef}></input>
           <Button type="submit">Add User</Button>
         </form>
       </Card>
